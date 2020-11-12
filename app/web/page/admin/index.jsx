fdf854962f8dac57406f943c5ffe4d1bd7b54042
joker <215537936@qq.com>
2020-11-13 00:33:00 +0800

    初始化项目

---
+++ app/web/page/admin/index.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { create } from './store';
+import Route from './router';
+
+import './index.css';
+
+export default class Admin extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      collapsed: false,
+    };
+  }
+  
+
+  // toggle = () => {
+  //   this.setState({
+  //     collapsed: !this.state.collapsed,
+  //   });
+  // }
+
+  render() {
+    const store = create(window.__INITIAL_STATE__);
+    const url = store.getState().url;
+    return (
+      <Provider store={ store }>
+        <BrowserRouter>
+          <Route url={ url }/>
+        </BrowserRouter>
+      </Provider>
+    );
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  target: 'web',
+  entry: {
+    login: 'app/web/page/login/index.jsx',
+    admin: 'app/web/page/admin/index.jsx'
+  }
+};
